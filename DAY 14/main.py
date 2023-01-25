6d8068503ef4ba066bb8d8fb7527be8fce7490a5
yiwereaminu <yiwereamin@gmail.com>
2023-01-25 18:09:46 +0000

    day 14 done

--- DAY 14/main.py
+++ DAY 14/main.py
@@ -9,7 +9,7 @@ if you guessed wrongly, the game ends
 
 
 """
-
+import os
 from random import randint
 import art
 import game_data
@@ -36,16 +36,22 @@ next_account()
 
 user_answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 while flag:
+    
     if (user_answer == 'a' and first_account() > next_account()) or (user_answer=='b' and (next_account() > first_account())):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(art.logo)
         score += 1
         rand = next_randint
         next_randint = randint(0,len(game_data.data)-1)
         print(f"You're right! Current score {score}")
+
         first_account()
         print(art.vs)
         next_account()
         user_answer = input("Who has more followers? Type 'A' or 'B': ").lower()
     else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(art.logo)
         print(f"Sorry, that's wrong. Final score: {score} ")
         flag = False
 
