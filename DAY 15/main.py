13ff75d8c4ea667c7d734a7992177d6ac804a1b0
yiwereaminu <yiwereamin@gmail.com>
2023-01-26 07:08:33 +0000

    day 15 todo 1 and 2 done

--- DAY 15/main.py
+++ DAY 15/main.py
@@ -1,1 +1,34 @@
-print('hello')
+import resources
+
+print(resources.MENU)
+print(resources.resources)
+# TODO 1 prompt the user to chose what they want
+
+user_choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+
+#  TODO 2 check if there is enough resources to serve the user
+def check_for_sufficient_resource(choice):
+    """this function checks if there are enough resources to make the beverage of choice"""
+    choice = user_choice
+    water = resources.MENU[choice]['ingredients']['water']
+    coffee = resources.MENU[choice]['ingredients']['coffee']
+    available_water = resources.resources['water']
+    available_milk = resources.resources['milk']
+    available_coffee = resources.resources['coffee']
+
+    if choice == 'latte' or choice == 'cappuccino':
+        milk = resources.MENU[choice]['ingredients']['milk']
+        if available_water < water:
+            print(f"Sorry there is not enough water")
+        elif available_coffee < coffee:
+            print('Sorry there not enough coffee')
+        elif available_milk < milk:
+            print("Sorry there is not enough milk")
+    elif choice == 'espresso':
+        if available_water < water:
+            print("Sorry there is not enough water")
+        elif available_coffee < coffee:
+            print("Sorry there is not enough coffee")
+
+
+check_for_sufficient_resource(user_choice)
